8ea48af4c6e39b2e00d263c899bc9ae22e48c347
Robert Mikhayelyan <romikh@microsoft.com>
2018-04-27 13:20:57 -0700

    Add project files.

---
+++ BasicCaptureSample/Capture.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Composition;
+
+namespace BasicCaptureSample
+{
+    class Capture : IDisposable
+    {
+        public Capture(CanvasDevice device, GraphicsCaptureItem item)
+        {
+            _item = item;
+            _device = device;
+
+            // TODO: Dpi?
+            _swapChain = new CanvasSwapChain(_device, item.Size.Width, item.Size.Height, 96);
+
+            _dispatcherQueueController = DispatcherQueueController.CreateOnDedicatedThread();
+            _dispatcherQueue = _dispatcherQueueController.DispatcherQueue;
+
+            // We don't want to return from the constructor untill the frame pool and
+            // the capture session are both initialized. We could do this on the UI thread,
+            // but you really shouldn't. This will update as fast as the screen refresh rate,
+            // so it would cause performance issues on the UI thread.
+            var wait = new AutoResetEvent(false);
+            var success = _dispatcherQueue.TryEnqueue(() =>
+            {
+                _framePool = Direct3D11CaptureFramePool.Create(
+                    _device,
+                    DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                    2,
+                    item.Size);
+                _session = _framePool.CreateCaptureSession(item);
+
+                _framePool.FrameArrived += OnFrameArrived;
+
+                wait.Set();
+            });
+
+            if (!success)
+            {
+                throw new Exception("Could not enqueue work!");
+            }
+
+            wait.WaitOne();
+        }
+
+        public void StartCapture()
+        {
+            _session.StartCapture();
+        }
+
+        public ICompositionSurface CreateSurface(Compositor compositor)
+        {
+            return CanvasComposition.CreateCompositionSurfaceForSwapChain(compositor, _swapChain);
+        }
+
+        public void Dispose()
+        {
+            _session?.Dispose();
+            _framePool?.Dispose();
+            _swapChain?.Dispose();
+
+            _swapChain = null;
+            _framePool = null;
+            _session = null;
+            _item = null;
+
+            if (_dispatcherQueueController != null)
+            {
+                var ignored = _dispatcherQueueController.ShutdownQueueAsync();
+                _dispatcherQueueController = null;
+                _dispatcherQueue = null;
+            }
+        }
+
+        private void OnFrameArrived(Direct3D11CaptureFramePool sender, object args)
+        {
+            using (var frame = sender.TryGetNextFrame())
+            {
+                using (var bitmap = CanvasBitmap.CreateFromDirect3D11Surface(_device, frame.Surface))
+                using (var drawingSession = _swapChain.CreateDrawingSession(Colors.Transparent))
+                {
+                    drawingSession.DrawImage(bitmap);
+                }
+            }
+            
+            _swapChain.Present();
+        }
+
+        private GraphicsCaptureItem _item;
+        private Direct3D11CaptureFramePool _framePool;
+        private GraphicsCaptureSession _session;
+        private SizeInt32 _lastSize;
+
+        private CanvasDevice _device;
+        private CanvasSwapChain _swapChain;
+
+        private DispatcherQueueController _dispatcherQueueController;
+        private DispatcherQueue _dispatcherQueue;
+    }
+}
---
+++ BasicCaptureSample/main.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Core;
+
+namespace BasicCaptureSample
+{
+    public class MainView : IFrameworkView
+    {
+        public void Initialize(CoreApplicationView applicationView)
+        {
+            _view = applicationView;
+        }
+
+        public void SetWindow(CoreWindow window)
+        {
+            _window = window;
+        }
+
+        public void Load(string entryPoint) { }
+
+        public void Run()
+        {
+            _compositor = new Compositor();
+            _target = _compositor.CreateTargetForCurrentView();
+            _root = _compositor.CreateSpriteVisual();
+            _content = _compositor.CreateSpriteVisual();
+            _brush = _compositor.CreateSurfaceBrush();
+
+            _root.Brush = _compositor.CreateColorBrush(Colors.White);
+            _root.RelativeSizeAdjustment = Vector2.One;
+            _target.Root = _root;
+
+            _content.AnchorPoint = new Vector2(0.5f, 0.5f);
+            _content.RelativeOffsetAdjustment = new Vector3(0.5f, 0.5f, 0);
+            _content.RelativeSizeAdjustment = Vector2.One;
+            _content.Size = new Vector2(-80, -80);
+            _content.Brush = _brush;
+            _brush.HorizontalAlignmentRatio = 0.5f;
+            _brush.VerticalAlignmentRatio = 0.5f;
+            _brush.Stretch = CompositionStretch.Uniform;
+            var shadow = _compositor.CreateDropShadow();
+            shadow.Mask = _brush;
+            _content.Shadow = shadow;
+            _root.Children.InsertAtTop(_content);
+
+            var ignored = StartCaptureAsync();
+
+            _window.Activate();
+            _window.Dispatcher.ProcessEvents(CoreProcessEventsOption.ProcessUntilQuit);
+        }
+
+        public void Uninitialize()
+        {
+            _compositor.Dispose();
+        }
+
+        private async Task StartCaptureAsync()
+        {
+            var picker = new Windows.Graphics.Capture.GraphicsCapturePicker();
+            var item = await picker.PickSingleItemAsync();
+            var device = new Microsoft.Graphics.Canvas.CanvasDevice();
+
+            _capture = new Capture(device, item);
+
+            var surface = _capture.CreateSurface(_compositor);
+            _brush.Surface = surface;
+
+            _capture.StartCapture();
+        }
+
+        private CoreWindow _window;
+        private CoreApplicationView _view;
+
+        private Compositor _compositor;
+        private CompositionTarget _target;
+        private SpriteVisual _root;
+        private SpriteVisual _content;
+        private CompositionSurfaceBrush _brush;
+
+        private Capture _capture;
+    }
+
+    public class MainViewFactory : IFrameworkViewSource
+    {
+        public IFrameworkView CreateView()
+        {
+            return new MainView();
+        }
+
+        public static void Main(string[] args)
+        {
+            CoreApplication.Run(new MainViewFactory());
+        }
+    }
+}
